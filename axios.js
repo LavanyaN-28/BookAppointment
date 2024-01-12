589385637774321e4744ef075db5de6f4b85a08c
LavanyaN-28 <152402758+LavanyaN-28@users.noreply.github.com>
2024-01-12 19:44:22 +0530

    updated axios.js file
    retrieving data using GET request when web page is reloaded.

--- axios.js
+++ axios.js
@@ -6,10 +6,10 @@ const userList = document.querySelector('#users');
 
 // Listen for form submit
 myForm.addEventListener('submit', onSubmit);
-
+var count=0
 function onSubmit(e) {
   e.preventDefault();
-  
+ 
   if(nameInput.value === '' || emailInput.value === '') {
     // alert('Please enter all fields');
     msg.classList.add('error');
@@ -18,9 +18,10 @@ function onSubmit(e) {
     // Remove error after 3 seconds
     setTimeout(() => msg.remove(), 3000);
   } else {
+    
     // Create new list item with user
     const li = document.createElement('li');
-    li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`))
+    const liText=document.createTextNode(nameInput.value)
     //DELETE BUTTON
     var deleteBtn=document.createElement('button');
     deleteBtn.className="deleteBtn"
@@ -33,6 +34,7 @@ function onSubmit(e) {
     editBtn.appendChild(document.createTextNode('Edit'));
     li.appendChild(editBtn)
    // Append to ul
+   showUsers(obj)
    userList.appendChild(li);
 
     const obj={
@@ -46,14 +48,34 @@ function onSubmit(e) {
     localStorage.setItem(obj.email, JSON.stringify(obj));
 
 
-    axios
-    .post('https://crudcrud.com/api/ed502b94419e4fd2a6c0fcbee7cdcdf7/appointmentData',obj)
-    .then(response => console.log(response.data))
+    axios.post('https://crudcrud.com/api/ed502b94419e4fd2a6c0fcbee7cdcdf7/appointmentData',obj)
+    .then((response) => {
+        console.log(response.data)
+    })
     .catch((err ) => {
         document.body.innerHTML=document.body.innerHTML + "<h4>Something went wrong</h4>";
-        console.error(err)
+        console.log(err)
     });
-}
 
+    window.addEventListener("DOMContentLoaded",() => {
+        axios.get('https://crudcrud.com/api/ed502b94419e4fd2a6c0fcbee7cdcdf7/appointmentData')
+        .then(response => {
+            for (var i=0;i<=response.data.length;i++){
+                showUsers(response.data[i])
+                console.log(response.data)}
+    })
+        .catch((err) =>{
+            document.body.innerHTML=document.body.innerHTML + "<h4> Cannot  get the details of the user";
+            console.log(err)
+        })
+    })
 }
+}
+function showUsers(user){
+    const childElemnt=document.createElement('li')
+    const edit=document.getElementById('edit')
+    childElemnt.textContent=user.name + ' : ' + user.email
+    userList.appendChild(childElemnt)
     
+    
+}
